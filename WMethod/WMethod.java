37ed676582b154b85c85c56fc6f0212bc31c9159
Malcolmt129 <malcolm.t129@gmail.com>
2023-05-01 16:34:10 -0400

    Task 1 and 2 finished and now have packages for the output and files

--- WMethod/WMethod.java
+++ WMethod/WMethod.java
@@ -381,12 +381,7 @@ public class WMethod{
      Vector <String> tests=generateTests(transitionCover, w); // Generate tests.
      Utilities.printAllTestCases(tests); // Print tests.
      
-     
-     // TODO: 	Write the necessary code to iterate through all test cases and run them against
-     // 		the FSM using the Utilities.runFSM() method. 
-     //
-     // Example use of the Utilities.runFSM() method
-      Utilities.runFSM(FSM, 1, tests, " ");
+    Utilities.runFSM(FSM, 1, tests, " ");
      
    }// End of main()
    
